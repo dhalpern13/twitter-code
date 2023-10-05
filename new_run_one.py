0634000d9c285aa251a9c2b61c318679184f2e4c
Daniel Halpern <dhalpern13@gmail.com>
2023-10-05 10:18:01 -0400

    Make test mode

--- new_run_one.py
+++ new_run_one.py
@@ -8,15 +8,15 @@ from twitter import compute_type_matrices, compute_limit_matrices, compute_engag
 
 all_ps = ['mode', 'beta-1-100-1', 'beta-1-100-2', 'uniform-1', 'uniform-2']
 set_scales = list(product([.1, .3, .9], [True])) + list(product([1, 3, 10, 30], [False]))
-steps = range(1, 11)
 
-param_combos = [(ps, set_scale, step) for ps, set_scale, step in product(all_ps, set_scales, steps)]
+
+param_combos = [(ps, set_scale) for ps, set_scale in product(all_ps, set_scales)]
 instance = int(argv[1])
-ps_name, (modify_val, is_set), step = param_combos[instance]
+ps_name, (modify_val, is_set) = param_combos[instance]
 is_set_str = "set" if is_set else "scale"
 ps = np.load(f'data/numpy_arrays/{ps_name}.npy')
 graph = np.load('data/numpy_arrays/graph.npy')
-print(f'{instance}: {ps_name}, {is_set_str}, {modify_val}, {step}')
+print(f'{instance}: {ps_name}, {is_set_str}, {modify_val}')
 
 if is_set:
     unscaled_type_matrices = compute_type_matrices(graph, ps, scale_by_following=False)
@@ -26,26 +26,25 @@ if is_set:
 else:
     scaled_ps = np.minimum(ps * modify_val, 0.99)
     type_matrices = compute_type_matrices(graph, scaled_ps, scale_by_following=True)
-# print('Computing matrix inverses...')
-# limit_matrices = compute_limit_matrices(type_matrices)
-#
-# print('Computing preliminaries...')
-# engagement_vectors = compute_engagement_vectors(limit_matrices, ps)
-#
-# num_types, *_ = ps.shape
-#
-# diversity = step / 40
-#
-# print('Computing opt...')
-# opt = opt_no_diversity(engagement_vectors)
-#
-# print('Computing diverse opts...')
-#
-# engagement = maximize_engagement(limit_matrices, engagement_vectors, diversity)
-# print(engagement)
+print('Computing matrix inverses...')
+limit_matrices = compute_limit_matrices(type_matrices)
 
-opt = np.random.uniform(1, 2)
-engagement = np.random.uniform(0, 1)
+print('Computing preliminaries...')
+engagement_vectors = compute_engagement_vectors(limit_matrices, ps)
 
-with open(f'diversities/{ps_name}-{is_set_str}-{modify_val}-{step}.txt', 'w') as f:
-    f.write(f'{engagement}\n{opt}\n{engagement / opt}')
+print('Computing opt...')
+opt = opt_no_diversity(engagement_vectors)
+
+num_types, *_ = ps.shape
+
+for step in range(1, 11):
+    diversity = step / 40
+
+    print(f'Computing divers opts {step}...')
+
+    # engagement = maximize_engagement(limit_matrices, engagement_vectors, diversity)
+    engagement = np.random.uniform(0, 1)
+    print(engagement)
+
+    with open(f'diversities/{ps_name}-{is_set_str}-{modify_val}-{step}.txt', 'w') as f:
+        f.write(f'{engagement}\n{opt}\n{engagement / opt}')
