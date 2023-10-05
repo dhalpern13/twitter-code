404968b4e8997754d327be5b827ae007cb72cdff
Daniel Halpern <dhalpern13@gmail.com>
2023-10-05 10:21:12 -0400

    Add tmux file

---
+++ tmux-multiple.py
@@ -0,0 +1,17 @@
+from itertools import product
+from math import ceil
+from os import system
+panes_per_window = 9
+instances = 35
+windows = ceil(instances / panes_per_window)
+
+for window in range(windows):
+    for _ in range(panes_per_window - 1):
+        system('tmux split-window')
+        system('tmux select-layout tiled')
+    if window < windows - 1:
+        system('tmux new-window')
+for instance in range(instances):
+    window = instance // panes_per_window
+    pane = instance % panes_per_window
+    system(f'tmux send-keys -t {window}.{pane} "python3 new_run_one.py {instance}" Enter')
